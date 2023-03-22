edbd0784e9de4d0df8c5963188d627563c1ca1fc
shivam-2306 <chaturvediji2306@gmail.com>
2023-03-23 00:13:17 +0530

    Third

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBWfRZi-3aiMEiHfkDatzNMpndn7ZOmeFE",
-    authDomain: "todo-shivam.firebaseapp.com",
-    databaseURL: "https://todo-shivam-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "todo-shivam",
-    storageBucket: "todo-shivam.appspot.com",
-    messagingSenderId: "762831452544",
-    appId: "1:762831452544:web:4fada42e2b6a00d12fe965",
-    measurementId: "G-F644LEWQ22"
+    apiKey: "AIzaSyAn_UXnLt02Ffh1kOXE0C2y86X-7weBQX0",
+    authDomain: "test-cfd99.firebaseapp.com",
+    databaseURL: "https://test-cfd99-default-rtdb.firebaseio.com",
+    projectId: "test-cfd99",
+    storageBucket: "test-cfd99.appspot.com",
+    messagingSenderId: "442320950003",
+    appId: "1:442320950003:web:7f34666286b16bfaabdd86",
+    measurementId: "G-WYX553T386"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+export const app = firebase.initializeApp(firebaseConfig);
+export var db = firebase.firestore();
+export const auth = firebase.auth();
+db.settings({ timestampsInSnapshots: true, merge: true });
--- script.js
+++ script.js
@@ -1,85 +1,130 @@
+import { auth, db } from './firebase.js'
+let curruser;
+const login = document.getElementById("login");
+const logout = document.getElementById("logout");
 
-function getItems(){
-    db.collection("todo-items").onSnapshot((snapshot) => {
-        let items = [];
-        snapshot.docs.forEach((doc) => {
-            items.push({
-                id: doc.id, 
-                ...doc.data()
-            })
-        })
-        generateItems(items);
-    })
+auth.onAuthStateChanged(user => {
+  console.log(user);
+  setupUI(user);
+  if (user) {
+    curruser = user;
+  }
+})
+
+const setupUI = (user) => {
+  if (user) {
+    logout.style.display = "block";
+    login.style.display = "none";
+  }
+  else {
+    logout.style.display = "none";
+    login.style.display = "block";
+  }
 }
 
-function generateItems(items){
-    let todoItems = [];
-    items.forEach((item) => {
-        let todoItem = document.createElement("div");
-        todoItem.classList.add("todo-item");
 
-        let checkContainer = document.createElement("div");
-        checkContainer.classList.add("check");
+const additem = document.getElementById("add-item");
+const allitems = document.getElementById("all-items");
+const actitems = document.getElementById("active");
+const compitems = document.getElementById("complete");
+const sort = document.getElementById("drop-down");
+const imp = document.getElementById("importance");
+const duedate = document.getElementById("duedate");
+const clear = document.getElementById("clear");
 
-        let checkMark = document.createElement("div");
-        checkMark.classList.add("check-mark");
-        checkMark.innerHTML = '<img src="./assets/icon-check.svg">';
-        checkMark.addEventListener("click", function(){
-            markCompleted(item.id);
-        })
-        checkContainer.appendChild(checkMark);
+function getItems() {
+  db.collection("todo-items").where("uid", "==", curruser.uid).onSnapshot((snapshot) => {
+    let items = [];
+    snapshot.docs.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+    generateItems(items);
+  })
+}
 
-        let todoTextContainer = document.createElement("div");
-        todoTextContainer.classList.add("todo-text-container");
+function generateItems(items) {
+  let todoItems = [];
+  items.forEach((item) => {
+    let todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
 
-        let todoText = document.createElement("div");
-        todoText.classList.add("todo-text");
-        todoText.innerText = item.text;
 
-        let timestampContainer = document.createElement("div");
-        timestampContainer.classList.add("timestamp-container");
+    let checkContainer = document.createElement("div");
+    checkContainer.classList.add("check");
 
-        let timestampText = document.createElement("div");
-        timestampText.classList.add("timestamp-text");
-        timestampText.innerText = new Date(item.timestamp).toLocaleString();
+    let checkMark = document.createElement("div");
+    checkMark.classList.add("check-mark");
+    checkMark.innerHTML = '<img src="./assets/icon-check.svg">';
+    checkMark.addEventListener("click", function () {
+      markCompleted(item.id);
+    })
+    checkContainer.appendChild(checkMark);
 
-        todoTextContainer.appendChild(todoText);
-        timestampContainer.appendChild(timestampText);
+    let todoTextContainer = document.createElement("div");
+    todoTextContainer.classList.add("todo-text-container");
 
-        let editButton = document.createElement("button");
-        editButton.classList.add("edit-button");
-        editButton.innerHTML = '<img src="./assets/icon-edit.svg">';
-        editButton.addEventListener("click", function(){
-            editItem(item.id);
-        });
+    let todoText = document.createElement("div");
+    todoText.classList.add("todo-text");
+    todoText.innerText = item.text;
 
-        let deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete-button");
-        deleteButton.innerHTML = '<img src="./assets/icon-delete.svg">';
-        deleteButton.addEventListener("click", function(){
-            deleteItem(item.id);
-        });
+    let timestampContainer = document.createElement("div");
+    timestampContainer.classList.add("timestamp-container");
 
-        todoItem.appendChild(checkContainer);
-        todoTextContainer.appendChild(timestampContainer);
-        todoTextContainer.appendChild(editButton);
-        todoTextContainer.appendChild(deleteButton);
-        todoItem.appendChild(todoTextContainer);
+    let timestampText = document.createElement("div");
+    timestampText.classList.add("timestamp-text");
+    timestampText.innerText = new Date(item.timestamp).toLocaleString().substring(0, 10);
 
-        if(item.status == "completed"){
-            checkMark.classList.add("checked");
-            todoText.classList.add("checked");
-        }
+    todoTextContainer.appendChild(todoText);
+    timestampContainer.appendChild(timestampText);
 
-        todoItems.push(todoItem);
+    let editButton = document.createElement("button");
+    editButton.classList.add("edit-button");
+    editButton.innerHTML = '<img src="./assets/icon-edit.svg">';
+    editButton.addEventListener("click", function () {
+      if (curruser && item.uid == curruser.uid)
+        editItem(item.id);
+      else {
+        console.log("you cannot edit this item");
+      }
     });
 
-    document.querySelector(".todo-items").replaceChildren(...todoItems);
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.innerHTML = '<img src="./assets/icon-delete.svg">';
+    deleteButton.addEventListener("click", function () {
+      if (curruser && item.uid == curruser.uid)
+        deleteItem(item.id);
+      else {
+        console.log("you cannot delete this item");
+      }
+    });
+
+    todoItem.appendChild(checkContainer);
+    todoTextContainer.appendChild(timestampContainer);
+    todoTextContainer.appendChild(editButton);
+    todoTextContainer.appendChild(deleteButton);
+    todoItem.appendChild(todoTextContainer);
+
+    if (item.status == "completed" && item.uid == curruser.uid) {
+      checkMark.classList.add("checked");
+      todoText.classList.add("checked");
+    }
+
+    if (item.important && item.uid == curruser.uid) todoText.classList.add("imp");
+
+    todoItems.push(todoItem);
+  });
+
+  document.querySelector(".todo-items").replaceChildren(...todoItems);
 }
 
 
-function addItem(event) {// console.log("TASKSTATUS");
-    event.preventDefault();
+additem.addEventListener("submit", function () {// console.log("TASKSTATUS");
+  event.preventDefault();
+  if (curruser) {
     let text = document.getElementById("todo-input");
     let dueDate = document.getElementById("due-date");
     let important = document.querySelector('.check-important').checked; // get the value of the checkbox
@@ -87,7 +132,8 @@ function addItem(event) {// console.log("TASKSTATUS");
       text: text.value,
       status: "active",
       timestamp: dueDate.value,
-      important: important // add important field to new item
+      important: important,
+      uid: curruser.uid// add important field to new item
     }).then((docRef) => {
       text.value = "";
       dueDate.value = "";
@@ -96,45 +142,56 @@ function addItem(event) {// console.log("TASKSTATUS");
       console.error("Error adding document: ", error);
     });
   }
+  else {
+    window.alert("you cannot add item. please login");
+  }
+  return false;
+})
 
-function markCompleted(id){// console.log("TASKSTATUS");
-    let item = db.collection("todo-items").doc(id);
-    item.get().then(function(doc) {
-        if (doc.exists) {
-            if(doc.data().status == "active"){
-                item.update({
-                    status: "completed"
-                })
-            } else {
-                item.update({
-                    status: "active"
-                })
-            }
+function markCompleted(id) {// console.log("TASKSTATUS");
+  let item = db.collection("todo-items").doc(id);
+
+  item.get().then(function (doc) {
+    if (doc.exists) {
+      if (doc.data().uid == curruser.uid) {
+        if (doc.data().status == "active") {
+          item.update({
+            status: "completed"
+          })
+        } else {
+          item.update({
+            status: "active"
+          })
         }
+      }
+      else {
+        console.log("you cannot perform this function");
+      }
+    }
+  })
+  getItems();
+}
+
+
+clear.addEventListener("click", function () {// console.log("TASKSTATUS");
+  db.collection("todo-items").where("status", "==", "completed").where("uid", "==", curruser.uid)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        doc.ref.delete();
+      });
     })
-    getItems();
+    .catch(function (error) {
+      console.error("Error removing completed tasks: ", error);
+    });
 }
+)
 
-
-function clearCompleted() {// console.log("TASKSTATUS");
-    db.collection("todo-items").where("status", "==", "completed")
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                doc.ref.delete();
-            });
-        })
-        .catch(function(error) {
-            console.error("Error removing completed tasks: ", error);
-        });
-}
-
-
-
-  
 function displayTasksByStatus(status) {// console.log("TASKSTATUS");
+  if (curruser) {
     db.collection("todo-items")
       .where("status", "==", status)
+      .where("uid", "==", curruser.uid)
       .get()
       .then((querySnapshot) => {
         let items = [];
@@ -150,91 +207,115 @@ function displayTasksByStatus(status) {// console.log("TASKSTATUS");
         console.log("Error getting documents: ", error);
       });
   }
-  
+}
 
-  function editItem(id) {// console.log("TASKSTATUS");
-    let item = db.collection("todo-items").doc(id);
-    let newText = prompt("Enter the new text for this todo item:");
-    item.update({
-        text: newText
-    })
-    .then(function() {
-        console.log("Todo item updated successfully!");
-    })
-    .catch(function(error) {
-        console.error("Error updating todo item: ", error);
-    });
+
+function editItem(id) {// console.log("TASKSTATUS");
+  let item = db.collection("todo-items").doc(id);
+  item.get().then(function (doc) {
+    if (doc.data().uid == curruser.uid) {
+      let newText = prompt("Enter the new text for this todo item:");
+      if (newText)
+        item.update({
+          text: newText
+        })
+          .then(function () {
+            console.log("Todo item updated successfully!");
+          })
+          .catch(function (error) {
+            console.error("Error updating todo item: ", error);
+          });
+    } else { console.log("you cannot perform this function") }
+  })
 }
 
 
 function deleteItem(itemId) {// console.log("TASKSTATUS");
-    const itemRef = db.collection('todo-items').doc(itemId);
-  
-    const confirmDelete = confirm("Are you sure you want to delete this item?");
-  
-    if (confirmDelete) {
-      itemRef.delete()
-        .then(() => {
-        
-          console.log("Item successfully deleted from database");
-          generateItems();
-        })
-        .catch((error) => {
-          console.error("Error deleting item from database: ", error);
-        });
-    }
-  }
-  function toggleDropdown() {// console.log("TASKSTATUS");
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  const itemRef = db.collection('todo-items').doc(itemId);
+
+  const confirmDelete = confirm("Are you sure you want to delete this item?");
+  itemRef.get().then(function (doc) {
+    if (doc.data().uid == curruser.uid)
+      if (confirmDelete) {
+        itemRef.delete()
+          .then(() => {
+
+            console.log("Item successfully deleted from database");
+            getItems();
+          })
+          .catch((error) => {
+            console.error("Error deleting item from database: ", error);
+          });
+      }
+      else { console.log("you cannot perform this function"); }
+  })
+}
+function toggleDropdown() {// console.log("TASKSTATUS");
+  document.getElementById("drop-down").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-  function sortByDueDate() {// console.log("TASKSTATUS");
-    db.collection("todo-items")
-      .orderBy("timestamp", "desc")
-      .get()
-      .then((querySnapshot) => {
-        const items = [];
-        querySnapshot.forEach((doc) => {
-          items.push({
-            id: doc.id,
-            ...doc.data(),
-          });
+}
+function sortByDueDate() {// console.log("TASKSTATUS");
+  db.collection("todo-items")
+    .orderBy("timestamp", "asc")
+    .where("uid", "==", curruser.uid)
+    .get()
+    .then((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({
+          id: doc.id,
+          ...doc.data(),
         });
-        generateItems(items);
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
       });
-  }
+      generateItems(items);
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+}
 
-  function sortByImportance() {// console.log("TASKSTATUS");
-    db.collection("todo-items")
-      .orderBy("important", "desc")
-      .get()
-      .then((querySnapshot) => {
-        const items = [];
-        querySnapshot.forEach((doc) => {
-          items.push({
-            id: doc.id,
-            ...doc.data(),
-          });
+function sortByImportance() {// console.log("TASKSTATUS");
+  db.collection("todo-items")
+    .orderBy("important", "desc")
+    .where("uid", "==", curruser.uid)
+    .get()
+    .then((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({
+          id: doc.id,
+          ...doc.data(),
         });
-        generateItems(items);
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
       });
-  }
-getItems();
+      generateItems(items);
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+}
+
+auth.onAuthStateChanged(user => {
+  console.log(user);
+})
+
+allitems.addEventListener("click", function () { getItems() });
+actitems.addEventListener("click", function () { displayTasksByStatus("active") });
+compitems.addEventListener("click", function () { displayTasksByStatus("completed") });
+sort.addEventListener("click", function () { toggleDropdown() });
+imp.addEventListener("click", function () { sortByImportance() });
+duedate.addEventListener("click", function () { sortByDueDate() });
+
+
+
